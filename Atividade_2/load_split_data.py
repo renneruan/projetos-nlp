9d39e695363b609775faf7c9f9f4dbd2de3c8f89
Rennê Ruan Alves Oliveira <renneruan@hotmail.com>
2024-11-12 23:51:15 -0300

    Documentação de código e informações no notebook

--- Atividade_2/load_split_data.py
+++ Atividade_2/load_split_data.py
@@ -10,6 +10,14 @@ from tqdm import tqdm
 
 
 class CorpusLoader:
+    """
+    Classe responsável por ler os documentos de texto e realizar a separação
+    entre dados de treino e teste
+
+    Parâmetros:
+    - type: tipo de tokenização da classe, se será word ou sentence.
+    """
+
     def __init__(self, type):
         self.type = type
         self.files_processed = []
@@ -22,6 +30,16 @@ class CorpusLoader:
             print("Divisão de tokens por sentenças")
 
     def read_files_chunk(self, files_path):
+        """
+        Realiza a leitura individual de cada arquivo e realiza a tokenização
+
+        Parâmetros:
+        - files_path: recebe lista de nomes dos arquivos a serem lidos.
+
+        Retornos:
+        - files: lista de arquivos lidos
+        - token_count: quantia de tokens lidos
+        """
         files = []
         token_count = 0
 
@@ -30,7 +48,11 @@ class CorpusLoader:
                 # print(f"Lendo arquivo: {file_path}")
                 file_data = json.load(file)
 
+                # Verifica se documento há texto
                 if file_data["text"]:
+
+                    # Realiza a verificação do tipo escolhido para realizar
+                    # a tokenização apropriada
                     if self.type == "word":
                         file_data["text_tokens"] = nltk.tokenize.word_tokenize(
                             file_data["text"], language="portuguese"
@@ -40,10 +62,15 @@ class CorpusLoader:
                             file_data["text"], language="portuguese"
                         )
 
+                    # Como na biblioteca NLTK não há os tokens de início/fim
+                    # estes são adicionados ao início e fim dos textos
                     file_data["text_tokens"].insert(0, "<start>")
                     file_data["text_tokens"].append("<end>")
 
+                    # Contabiliza a quantia de tokens por documento
                     file_data["token_count"] = len(file_data["text_tokens"])
+
+                    # Somatório total da quantia de tokens dos documentos lidos
                     token_count += file_data["token_count"]
                 else:
                     file_data["text_tokens"] = []
@@ -54,15 +81,27 @@ class CorpusLoader:
         return files, token_count
 
     def read_all_files(self, corpus_directory, files_quantity):
+        """
+        Leitura de nomes de arquivos presentes no diretório informado
+
+        Parâmetros:
+        - corpus_directory: Diretório do corpo de texto que será utilizado
+        - files_quantity: quantia de arquivos presente no diretório que será lida
+
+        Retornos:
+        - files_processes: arquivos lidos e com devidos valores calculados
+        """
         files_names = [
             os.path.join(corpus_directory, file)
             for file in os.listdir(corpus_directory)
             if file.endswith(".json")
         ]
 
+        # Verifica se há quantidade delimitante
         if files_quantity != None:
             files_names = files_names[:files_quantity]
 
+        # Lê o bloco de arquivos a partir da lista de nomes
         print(f"Lendo {len(files_names)} arquivos.")
         files, token_count = self.read_files_chunk(files_names)
         self.files_processed.extend(files)
@@ -71,6 +110,16 @@ class CorpusLoader:
         return self.files_processed
 
     def split_test_train(self, split_proportion):
+        """
+        Separação de dados de treino e teste
+
+        Parâmetros:
+        - split_proportion: proporção de dados de treino a ser utilizada
+
+        Retornos:
+        - train_data: Lista de tokens de treino
+        - test_data: Lista de tokens de teste
+        """
         proportional_token_count = split_proportion * self.total_token_count
 
         train_data = []
@@ -78,16 +127,24 @@ class CorpusLoader:
 
         count_tokens = 0
 
+        # Embaralha os arquivos lidos em uma ordem aleatória
         shuffled_files_processed = self.files_processed
         random.seed(4)
         random.shuffle(shuffled_files_processed)
 
         print("Separando pedaços de teste e treino, aguarde...")
         for file in tqdm(shuffled_files_processed):
+
+            # Como cada arquivo pode ter uma quantia de tokens diferente
+            # para obter uma melhor porcentagem de separação vamos separar a porcentagem
+            # escolhida por tokens e não arquivos
+
+            # Calculamos a quantidade de tokens até o limiar desejado
             if count_tokens + file["token_count"] <= proportional_token_count:
                 train_data += file["text_tokens"]
                 count_tokens += file["token_count"]
             else:
+                # O restante colocamos em dados de treino
                 test_data += file["text_tokens"]
 
         print(f"Tamanho de texto de teste: {len(train_data)}")
